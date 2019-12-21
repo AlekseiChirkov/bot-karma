6fabf66ab217dfa6ea5a19792d84e7e93a46904a
AlekseiChirkov <tektonikboy98@gmail.com>
2019-12-21 13:26:15 +0600

    ok

--- project_folders/karma/slack.py
+++ project_folders/karma/slack.py
@@ -1,6 +1,14 @@
-import sys
 import telepot
 
-from decouple import config
 from slacker import Slacker
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from ..slack.models import Channel
+from ..slack.utils import get_missed_member_telegram_handles
+
+slack = Slacker('xoxb-865126114002-881145910964-YiQ1vJ7ClNONwXBlrDzAz50m')
+
+class Service:
+    @classmethod
+    def get_bot(cls):
+        return telepot.Bot(settings.BOT_TOKEN)
